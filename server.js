b129407f60bc6a6d52f1b85708f6eb0ca76aa6d8
gmireles210 <44076044+gmireles210@users.noreply.github.com>
2019-12-17 16:31:49 -0800

    creating files

--- server.js
+++ server.js
@@ -1,17 +1,46 @@
 'use strict';
+
 //Constructors//
 const express = require('express');
-const app = express();
+const ejs = require('ejs');
 const superagent = require('superagent');
 const PORT = process.env.PORT || 3000;
-let ejs = require('ejs');
-app.set('view engine', 'ejs');
 
 require('dotenv').config();
 
+const app = express();
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('./public'));
+
+app.set('view engine', 'ejs');
+
 app.get('/hello', (req,res) => {
   res.render('pages/index')
-}) 
+});
+
+app.post('/', (req, res) => {
+  superagent.get(`https://www.googleapis.com/books/v1/volumes?q=author+inauthor:${req.body.author}`).then(data => {
+
+    const books = data.body.items.map(book => ({name: book.volumeInfo.title}));
+
+    console.log(books);
+
+    res.render('book-results', {
+      books: books
+    });
+  });
+
+
+});
+
+
+
+
+
+
+
+
+
 
 
 app.listen(3000, () => {console.log('Port 3000 is connected');});

7d6f434fd10e3eb5b07598533108a784332c5beb
gmireles210 <44076044+gmireles210@users.noreply.github.com>
2019-12-18 12:22:09 -0800

    updated search functions for author and title

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const superagent = require('superagent');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use(express.static('./public'));
+app.use(express.static('public'));
 const PORT = process.env.PORT || 3000;
 
 require('dotenv').config();
@@ -19,13 +19,15 @@ app.use(express.urlencoded({extended: true}));
 
 
 app.get('/', (req, res) => {
-  superagent.get(`https://www.googleapis.com/books/v1/volumes/?q=${ 'Star Wars'}`)
+  let query = 'Star Wars';
+  if (req.query.public) {
+    query = req.query.public;
+  }
+  superagent.get(`https://www.googleapis.com/books/v1/volumes/?q=${ query }`)
     .then(bookResponse => {
       //console.log(bookResponse);
       res.render('./pages/index.ejs', {books: bookResponse.body.items});
     });
-
-
 
 
 
